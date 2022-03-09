0a72706d0f1692ed434e61f0bc0e822216aa5506
Lowtroo <xieyuhuai@outlook.com>
2022-03-09 13:31:40 +0800

    improve experience with emacsclient in nw mode

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,17 +1,37 @@
 (require 'kaolin-themes)
 (load-theme 'kaolin-galaxy t)
+(require 'pyim)
+(require 'pyim-basedict)
 
-(set-language-environment "UTF-8")
-(set-default-coding-systems 'utf-8)
-(set-buffer-file-coding-system 'utf-8-unix)
-(set-clipboard-coding-system 'utf-8-unix)
-(set-file-name-coding-system 'utf-8-unix)
-(set-keyboard-coding-system 'utf-8-unix)
-(set-next-selection-coding-system 'utf-8-unix)
-(set-selection-coding-system 'utf-8-unix)
-(set-terminal-coding-system 'utf-8-unix)
-(setq locale-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+(defun my-set-terminal-coding-system (&optional frame)
+  "Force ther terminal use proper coding system of utf-8"
+  (with-selected-frame (or frame (selected-frame))
+    (unless window-system
+      (set-language-environment "UTF-8")
+      (prefer-coding-system 'utf-8)
+
+;;      (set-default-coding-systems 'utf-8)
+      (set-keyboard-coding-system 'utf-8-unix)
+      (set-terminal-coding-system 'utf-8-unix)
+      
+      (set-buffer-file-coding-system 'utf-8-unix)
+
+;;      (set-clipboard-coding-system 'utf-8-unix)  for x window
+
+
+      ;; (set-next-selection-coding-system 'utf-8-unix) ;; for x window 
+      ;; (set-selection-coding-system 'utf-8-unix) ;; for x window 
+
+      (setq locale-coding-system 'utf-8)
+      
+      (pyim-basedict-enable)
+      (setq default-input-method "pyim")
+      (pyim-default-scheme 'quanpin)
+      ;; 显示5个候选词
+      (setq pyim-page-length 5))))
+
+(my-set-terminal-coding-system)
+(add-hook 'after-make-frame-functions 'my-set-terminal-coding-system)
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
