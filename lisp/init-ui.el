301c6b860e6455d10213e44523367bbb140182f2
Lowtroo <xieyuhuai@outlook.com>
2022-03-09 21:13:11 +0800

    coding system configurations fix

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -10,15 +10,12 @@
       (set-language-environment "UTF-8")
       (prefer-coding-system 'utf-8)
 
-;;      (set-default-coding-systems 'utf-8)
+      (set-default-coding-systems 'utf-8)
       (set-keyboard-coding-system 'utf-8-unix)
       (set-terminal-coding-system 'utf-8-unix)
       
       (set-buffer-file-coding-system 'utf-8-unix)
-
-;;      (set-clipboard-coding-system 'utf-8-unix)  for x window
-
-
+      
       ;; (set-next-selection-coding-system 'utf-8-unix) ;; for x window 
       ;; (set-selection-coding-system 'utf-8-unix) ;; for x window 
 
